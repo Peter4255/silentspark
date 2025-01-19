7954998e06d6b54fefef53717ea96a5e5db43b97
Peter4255 <peterrafai42@gmail.com>
2025-01-19 09:59:30 +0500

    Add files via upload
    http://whatplusapp.com/

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    // Set the page content type to HTML
+    header('Content-Type: text/html; charset=UTF-8');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Go to WhatsApp Plus</title>
+</head>
+<body>
+    <h1>Welcome!</h1>
+    <p>Click the link below to visit WhatsApp Plus:</p>
+    <a href="http://whatplusapp.com/" target="_blank" rel="noopener noreferrer">
+        Visit WhatsApp Plus
+    </a>
+</body>
+</html>
